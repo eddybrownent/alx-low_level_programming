bcc48e3e3c49191c4553546eb858ab1e4ae76ace
eddybrownent <ekaranja.28@gmail.com>
2023-02-17 22:47:35 +0300

    prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,27 +3,26 @@
 /**
  * main - Entry point
  *
- * Description: program that prints all possible different
+ * Description : program that prints all possible different
  * combinations of two digits
  *
- * Return: Always 0 (Success)
+ * Return : Always 0 (Success)
  *
  */
 int main(void)
 {
   int c;
   int d = 0;
-  while (d < 10)
+  while(d < 10)
     {
       c = 0;
-      while (c < 10)
+      while(c < 10)
 	{
-	  if (d != c && d < c)
+	  if(d != c && d < c)
 	    {
 	      putchar('0'+ d);
 	      putchar('0'+ c);
-
-		if (c + d != 17)
+		if(c + d != 17)
 		  {
 		    putchar(',');
 		    putchar(' ');
