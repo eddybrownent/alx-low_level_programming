a36ed7b78dda62506ef5e44f18cb8f79debddbb8
eddybrownent <ekaranja.28@gmail.com>
2023-02-21 22:22:40 +0300

    prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
-* main - prints the first 98 fibonacci numbers, starting
-* with 1 and 2
-* Return: 0 (Success)
+* main - main block
+* Description: Find and print the first 98 fib numbers starting with 1 and 2.
+* Numbers should be coma and space separated.
+* Return: 0
 */
 int main(void)
 {
+unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+unsigned long int hold1, hold2, hold3;
 int count;
-unsigned long f1 = 0, f2 = 1, sum;
-unsigned long f1_hf1, f1_hf2, f2_hf1, f2_hf2;
-unsigned long hf1, hf2;
 
-for (count = 0; count < 92; count++)
+printf("%lu, %lu, ", bk1, bk2);
+for (count = 2; count < 98; count++)
 {
-sum = f1 + f2;
-printf("%lu, ", sum);
-f1 = f2;
-f2 = sum;
+if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+{
+hold1 = (bk1 + bk2) / LARGEST;
+hold2 = (bk1 + bk2) % LARGEST;
+hold3 = fr1 + fr2 + hold1;
+fr1 = fr2, fr2 = hold3;
+bk1 = bk2, bk2 = hold2;
+printf("%lu%010lu", fr2, bk2);
 }
-f1_hf1 = f1 / 1000000000;
-f2_hf1 = f2 / 1000000000;
-f1_hf2 = f1 % 1000000000;
-f2_hf2 = f2 % 1000000000;
-for (count = 93; count < 99; count++)
+else
 {
-hf1 = f1_hf1 + f2_hf1;
-hf2 = f1_hf2 + f2_hf2;
-if (f1_hf2 + f2_hf2 > 999999999)
-{
-hf1 += 1;
-hf2 += 1000000000;
+hold2 = bk1 + bk2;
+bk1 = bk2, bk2 = hold2;
+printf("%lu", bk2);
 }
-printf("%lu%lu", hf1, hf2);
-if (count != 98)
-printf(",");
-f1_hf1 = f2_hf1;
-f1_hf2 = f2_hf2;
-f2_hf1 = hf1;
-f2_hf2 = hf2;
+if (count != 97)
+printf(", ");
 }
 printf("\n");
 return (0);
