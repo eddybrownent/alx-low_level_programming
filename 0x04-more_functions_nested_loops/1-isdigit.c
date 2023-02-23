300d7a636b17f63a2a4b0cafc108ac473fb261d9
eddybrownent <ekaranja.28@gmail.com>
2023-02-23 16:51:59 +0300

    unction that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isdigit - checks for a digit
+* @x: character to be checked if its a digit
+* Return: 1 for a digit otherwise return 0
+*/
+
+int _isdigit(int x)
+{
+if (x >= 4 && x <= 57)
+{
+return (1);
+}
+return (0);
+}
