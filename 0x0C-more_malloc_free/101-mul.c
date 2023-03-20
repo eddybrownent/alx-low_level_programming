7829ab10346975453a0dc27fce89f7dab466944f
eddybrownent <ekaranja.28@gmail.com>
2023-03-20 20:37:09 +0300

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#define ERR_MSG "Error"
 
 int find_len(char *str);
 char *create_xarray(int size);
@@ -8,7 +11,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
 /**
 * find_len - Finds the length of a string.
 * @str: The string to be measured.
-*
 * Return: The length of the string.
 */
 int find_len(char *str)
