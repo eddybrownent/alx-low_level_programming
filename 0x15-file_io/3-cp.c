1ce86284b897c951abf0ad090a6cab81b9b1b5f5
eddybrownent <ekaranja.28@gmail.com>
2023-04-29 12:27:06 +0300

    modified 3-pc files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
 
-void check_st(int st, int file_des, char *file_n, char type);
+char *c_buf(char *file);
+void cls_file(int fd);
+
 /**
- * main - copies the content of one file to another
- * @argc: argument count
- * @argv: arguments passed
- *
- * Return: 1 on success, exit otherwise
- */
+* create_buffer - allocates byte for a buffer.
+* @file: name of file buffer is storing chars for.
+*
+* Return: a pointer to the buffer.
+*/
+
+char *c_buf(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buf);
+}
+
+/**
+* main - copies the contents of a file to another file.
+* @argc: no of arguments given to the program
+* @argv: the array of pointers to the arguments.
+*
+* Return: 0 on success.
+*/
+
 int main(int argc, char *argv[])
 {
-	int shr, destiny, num_read = 1024, wrote, close_shr, close_destiny;
-	unsigned int type = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	char buf[1024];
+	int src, des, x, i;
+	char *buf;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	shr = open(argv[1], O_RDONLY);
-	check_st(shr, -1, argv[1], 'O');
-	destiny = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, type);
-	check_st(destiny, -1, argv[2], 'W');
-	while (num_read == 1024)
-	{
-		num_read = read(shr, buf, sizeof(buf));
-		if (num_read == -1)
-			check_st(-1, -1, argv[1], 'O');
-		wrote = write(destiny, buf, num_read);
-		if (wrote == -1)
-			check_st(-1, -1, argv[2], 'W');
-	}
-	close_shr = close(shr);
-	check_st(close_shr, shr, NULL, 'C');
-	close_destiny = close(destiny);
-	check_st(close_destiny, destiny, NULL, 'C');
+
+	buf = c_buf(argv[2]);
+	src = open(argv[1], O_RDONLY);
+	x = read(src, buf, 1024);
+	des = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (src == -1 || x == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+		i = write(des, buf, x);
+		if (des == -1 || i == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buf);
+			exit(99);
+		}
+		x = read(src, buf, 1024);
+		des = open(argv[2], O_WRONLY | O_APPEND);
+	} while (x > 0);
+
+	free(buf);
+	cls_file(src);
+	cls_file(des);
+
 	return (0);
 }
 
 /**
- * check_st - cle can be opened or closed
- * @st: file descriptor of the file to be opened
- * @file_n: name of the file
- * @type: closing or opening
- * @file_des: file descriptor
- *
- * Return: void
- */
-void check_st(int st, int file_des, char *file_n, char type)
+* cls_file - closes file descriptors.
+* @fd: file descriptors to be closed.
+* Return: nothing
+*/
+
+void cls_file(int fd)
 {
-	if (type == 'C' && st == -1)
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant close file_des %d\n", file_des);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
-	}
-	else if (type == 'O' && st == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant read file %s\n", file_n);
-		exit(98);
-	}
-	else if (type == 'W' && st == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant write  %s\n", file_n);
-		exit(99);
 	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/st.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
