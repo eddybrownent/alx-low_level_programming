6125453790235268b6f898d075477451325b764d
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-11 16:55:39 +0300

    fixing to the correct output of the code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if arg_c == 0:
         print("0 arguments.")
     elif arg_c == 1:
-        print("1 argument.")
+        print("1 argument:")
         print("{}: {}".format(arg_c, sys.argv[1]))
     else:
         print("{} arguments:".format(arg_c))
