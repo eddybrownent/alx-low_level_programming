3a3e4d0bef54e092b9995975da142d6077f01487
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-12 12:36:09 +0300

    program that imports all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,3 +21,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
     elif sys.argv[2] == '/':
         print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+    sys.exit(1)
