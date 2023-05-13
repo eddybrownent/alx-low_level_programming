ed9b0c16311145ebedd4ec03384b012656053c01
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-13 14:11:43 +0300

    fixing the code output and pycodesytle requirements

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     if my_list:
         result = []  # Initialize an empty list to store Boolean values
         for num in my_list:
-            result.append(num % 2 == 0)  # Append True if divisible by 2, False otherwise
+            result.append(num % 2 == 0)  # Append True if divisible by 2
         return result
     else:
         return None  # Return None for an empty list
