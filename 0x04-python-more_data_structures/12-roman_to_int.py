0c4b6eace1e2087d66d8d6648d4e8257ca3bf3a5
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-18 00:55:22 +0300

    function that converts a roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0
 
-    rom_n = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     num = 0
     last_roma = float('inf')
@@ -15,7 +15,7 @@ def roman_to_int(roman_string):
         curr_roma = rom_n[char]
 
         if curr_roma > last_roma:
-            num += curr_roma -2 * last_roma
+            num += curr_roma - 2 * last_roma
         else:
             num += curr_roma
 
