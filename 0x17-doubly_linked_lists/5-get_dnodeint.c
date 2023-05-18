bb70f11b5b76364ac089b66b972f39166c557d33
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-18 21:03:40 +0300

    fixing the output of this function

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,12 +8,19 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+
 	if (head == NULL)
 		return (NULL);
 	while (head->prev != NULL)
 		head = head->prev;
 
-	for (; index != 0; index--)
+	while (head)
+	{
+		if (i == index)
+			break;
 		head = head->next;
+		i++;
+	}
 	return (head);
 }
