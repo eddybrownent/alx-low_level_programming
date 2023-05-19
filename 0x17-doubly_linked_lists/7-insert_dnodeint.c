e80fc9e9f05de09a764b4195ec07e31b258de157
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-19 11:23:12 +0300

    fuction to add a node at a nth postion in a double linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (head->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	while (head->next)
+	while (head->next != NULL)
 		head = head->next;
 
 	for (; idx != 1; idx--)
