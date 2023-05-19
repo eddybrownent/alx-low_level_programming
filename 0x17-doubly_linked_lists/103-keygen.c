34a8ffdfb2e756002094020935c745fb07be7932
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-19 17:10:18 +0300

    keygen for crackme5

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * generatePassword - Generate  password for the given username.
+ * @username: The username for which to generate the password.
+ * @password: The buffer to store the generated password.
+ */
+void generatePassword(const char *username, char *password)
+{
+    const char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+    int len = strlen(username);
+    int i, temp;
+
+    temp = (len ^ 59) & 63;
+    password[0] = codex[temp];
+
+    temp = 0;
+    for (i = 0; i < len; i++)
+        temp += username[i];
+    password[1] = codex[(temp ^ 79) & 63];
+
+    temp = 1;
+    for (i = 0; i < len; i++)
+        temp *= username[i];
+    password[2] = codex[(temp ^ 85) & 63];
+
+    temp = 0;
+    for (i = 0; i < len; i++)
+    {
+        if (username[i] > temp)
+            temp = username[i];
+    }
+    srand(temp ^ 14);
+    password[3] = codex[rand() & 63];
+
+    temp = 0;
+    for (i = 0; i < len; i++)
+        temp += (username[i] * username[i]);
+    password[4] = codex[(temp ^ 239) & 63];
+
+    for (i = 0; i < username[0]; i++)
+        temp = rand();
+    password[5] = codex[(temp ^ 229) & 63];
+
+    password[6] = '\0';
+}
+
+/**
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+    char password[7];
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <username>\n", argv[0]);
+        return 1;
+    }
+
+    generatePassword(argv[1], password);
+    printf("%s\n", password);
+
+    return 0;
+}
