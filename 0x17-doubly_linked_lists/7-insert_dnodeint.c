7855ae3738acf0e8fd7d9145c750d8c2069590d7
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-19 11:09:38 +0300

    fuction to add a node at a nth postion in a double linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,13 +1,18 @@
 #include "lists.h"
 
 /**
+ * insert_dnodeint_at_index - adds a node at nth postion
+ * @h: pointer t the head of the list
+ * @n: the integer in the node
+ * @idx: the index at which the node will insert
  *
+ * Return: addres of the new node or NULL if fail
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *head, *new;
-	head = *h;
 
+	head = *h;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
@@ -17,6 +22,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (head->next == NULL)
 		return (add_dnodeint_end(h, n));
+
+	while (head->prev)
+		head = head->prev;
+
 	for (; idx != 1; idx--)
 	{
 		head = head->next;
@@ -31,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	head->next = new;
 
 
-	return new;
+	return (new);
 
 
 
