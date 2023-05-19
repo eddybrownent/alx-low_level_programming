e245f1d7bd570066233689a31f63723b7b1dfb2f
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-19 17:25:52 +0300

    keygen for crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -9,42 +9,43 @@
  */
 void generatePassword(const char *username, char *password)
 {
-    const char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    int len = strlen(username);
-    int i, temp;
+	const char *codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	int len = strlen(username);
+	int i, temp;
 
-    temp = (len ^ 59) & 63;
-    password[0] = codex[temp];
+	temp = (len ^ 59) & 63;
+	password[0] = codex[temp];
 
-    temp = 0;
-    for (i = 0; i < len; i++)
-        temp += username[i];
-    password[1] = codex[(temp ^ 79) & 63];
+	temp = 0;
+	for (i = 0; i < len; i++)
+		temp += username[i];
+	password[1] = codex[(temp ^ 79) & 63];
 
-    temp = 1;
-    for (i = 0; i < len; i++)
-        temp *= username[i];
-    password[2] = codex[(temp ^ 85) & 63];
+	temp = 1;
 
-    temp = 0;
-    for (i = 0; i < len; i++)
-    {
-        if (username[i] > temp)
-            temp = username[i];
-    }
-    srand(temp ^ 14);
-    password[3] = codex[rand() & 63];
+	for (i = 0; i < len; i++)
+		temp *= username[i];
+	password[2] = codex[(temp ^ 85) & 63];
 
-    temp = 0;
-    for (i = 0; i < len; i++)
-        temp += (username[i] * username[i]);
-    password[4] = codex[(temp ^ 239) & 63];
+	temp = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (username[i] > temp)
+			temp = username[i];
+	}
+	srand(temp ^ 14);
+	password[3] = codex[rand() & 63];
 
-    for (i = 0; i < username[0]; i++)
-        temp = rand();
-    password[5] = codex[(temp ^ 229) & 63];
+	temp = 0;
+	for (i = 0; i < len; i++)
+		temp += (username[i] * username[i]);
+	password[4] = codex[(temp ^ 239) & 63];
 
-    password[6] = '\0';
+	for (i = 0; i < username[0]; i++)
+		temp = rand();
+	password[5] = codex[(temp ^ 229) & 63];
+
+	password[6] = '\0';
 }
 
 /**
@@ -56,16 +57,17 @@ void generatePassword(const char *username, char *password)
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    char password[7];
+	char password[7];
 
-    if (argc != 2)
-    {
-        printf("Usage: %s <username>\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Usage: %s <username>\n", argv[0]);
+		return (1);
+	}
 
-    generatePassword(argv[1], password);
-    printf("%s\n", password);
+	generatePassword(argv[1], password);
+	printf("%s\n", password);
 
-    return 0;
+	return (0);
+
 }
