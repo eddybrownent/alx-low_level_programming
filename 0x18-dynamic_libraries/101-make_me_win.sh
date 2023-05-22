ad0782b17e25ec836bf17aab9fb73fdc07262698
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-22 21:45:32 +0300

    code injection to win jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Download the raw file
 wget -P /tmp/ https://raw.githubusercontent.com/eddybrownent/alx-low_level_programming/master/0x18-dynamic_libraries/libinjection.so
-
-# Set the LD_PRELOAD environment variable
 export LD_PRELOAD="/tmp/libinjection.so"
