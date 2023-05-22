0aa42d4d586a5c9b42d598808766ae0842920838
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-22 19:49:50 +0300

    functions that print some basic info about Python lists and Python bytes objects

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -27,18 +27,22 @@ void print_python_bytes(PyObject *p)
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
 
-	limit = (size >= 10) ? 10 : size + 1;
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
+
 
 	printf("  first %ld bytes:", limit);
 	for (i = 0; i < limit; i++)
 	{
 		char value = string[i];
 
-		if (value >= 0)
-			printf(" %02x", value);
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
 		else
-			printf(" %02x", 256 + value);
-	}
+			printf(" %02x", 256 + string[i]);
+
 	printf("\n");
 }
 
