e36780873b4f8dd069fac9c25bbe166c7d3e35b4
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-05-30 18:19:52 +0300

    fixing the documentation requrements

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 class Square:
-    """ This class represents a square.
-    Attributes: _size the size of the square(private)
+    """
+    This class represents a square.
+    Attributes:
+    _size : the size of the square (private)
     """
     def __init__(self, size):
         """
-        initializes a new instance of the Square calss
-        Args: size the size of the square
+        initializes a new instance of the Square class
+        Args:
+        size: the size of the square.
         """
         self.__size = size
