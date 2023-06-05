739abb7f087ec83a7601623a748a80b8819aa608
edwin karanja mburu <edwin@eddy-kali.browns.com>
2023-06-05 18:08:07 +0300

    fixing the errors

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,11 @@
-#!/usr/python3
-"""module to define an empty class"""
+#!/usr/bin/python3
+"""
+module to define an empty class
+"""
 
 
 class Rectangle:
-    """empty class Rectangle that represents  a rectangle"""
+    """
+    empty class Rectangle that represents  a rectangle
+    """
     pass
